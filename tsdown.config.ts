9bf9e99a8b910a13e5176ce153be6dbbc00002bb
Baptiste Fontaine <baptiste@bixoto.com>
2025-08-26 12:04:33 +0200

    Add utils into the build

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,7 +1,7 @@
 import {type Config, defineConfig} from 'tsdown'
 
 const config: Config = defineConfig({
-    entry: ['./src/index.ts'],
+    entry: ['./src/index.ts', './src/utils.ts'],
     format: 'esm',
     target: 'node18.12',
     clean: true,
